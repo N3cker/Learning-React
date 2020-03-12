9a3893b2fb8acf3c927f6318ef4e298884847898
Mateusz Piasecki <piaszuu@gmail.com>
2020-03-12 21:56:01 +0100

    Add Column Creator

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,17 +3,41 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore.js';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js';
+
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
   static propTypes = {
     title: PropTypes.node,
-    children: PropTypes.node.isRequired,
     image: PropTypes.string,
     titleColumn: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
+  }
+
+  addColumn(title){
+    this.setState(state => (
+      {
+        columns: [
+          ...state.columns,
+          {
+            key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+            title,
+            icon: 'list-alt',
+            cards: []
+          }
+        ]
+      }
+    ));
   }
 
   render() {
@@ -23,13 +47,17 @@ class List extends React.Component {
               imageText={this.props.image} />
 
         <div className={styles.description}>
-          {this.props.children}
+          {ReactHtmlParser(this.props.description)}
         </div>
 
         <div className={styles.columns}>
-          <Column titleColumn={'col1'}/>
-          <Column titleColumn={'col2'}/>
-          <Column titleColumn={'col3'}/>
+          {this.state.columns.map(({key, ...columnProps}) => (
+          <Column key={key} {...columnProps} />
+          ))}
+        </div>
+
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
 
       </section>
